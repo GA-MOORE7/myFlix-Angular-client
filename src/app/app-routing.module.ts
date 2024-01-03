71e63d5b265231aed7a3b3e5c6adc45b91dc9e10
Gerald Arthur Moore <gerald.moore42@gmail.com>
2024-01-03 02:38:43 +0200

    Create a dialog box and link it to the service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+// import { Routes } from '@angular/router';
+
+// export const routes: Routes = [];
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 export const routes: Routes = [];
+
+@NgModule({
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
+
