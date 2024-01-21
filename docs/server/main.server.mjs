73e70f58d033dec890d249195a5cbce959087c10
Gerald Arthur Moore <gerald.moore42@gmail.com>
2024-01-21 22:53:36 +0200

    User registration edit

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-A7WDTXB6.mjs";import"./chunk-LFMAX4DR.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-P4VAF5YN.mjs";import"./chunk-LFMAX4DR.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
